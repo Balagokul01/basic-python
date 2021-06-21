2b2796995ae25239d9df6f9da6fc45f93ad7b746
Balagokul <84243841+Balagokul01@users.noreply.github.com>
2021-06-21 17:31:13 +0530

    Create Quick sort.py
    Quick_sort algorithm with user input.

---
+++ Sorting/Quick sort.py
@@ -0,0 +1,22 @@
+#defining function quick sort and accepting numbers to sort from users
+
+def quick_sort(collection: list) -> list:
+	
+    if len(collection) < 2:
+        return collection
+    pivot = collection.pop()  # Use the last element as the first pivot
+    greater: List[int] = []  # All elements greater than pivot
+    lesser: List[int] = []  # All elements less than or equal to pivot
+    for element in collection:
+        (greater if element > pivot else lesser).append(element)
+    return quick_sort(lesser) + [pivot] + quick_sort(greater)
+    
+    
+    
+if __name__ == "__main__":
+    # taking user input 
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+ 
+    unsorted = [int(item) for item in user_input.split(",")]
+
+    print(quick_sort(unsorted)) # defined function is called 
