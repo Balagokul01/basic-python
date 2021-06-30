ffca2a52a681425bcc18e76e0be5f467bdad943a
Balagokul <84243841+Balagokul01@users.noreply.github.com>
2021-06-30 20:03:36 +0530

    Create If elif.py

---
+++ Control Structures/If statement/If elif.py
@@ -0,0 +1,29 @@
+#if elif statement
+
+x = 3
+if x == 2:
+#in this if statement condition is that x should be equal to 2   
+  print('two')
+elif x == 3:
+#in this if statement condition is that x should be equal to 2  
+     print('three')
+elif x == 4:
+#in this if statement condition is that x should be equal to 4       
+     print('four')
+else:
+     print('something else')
+
+#output: 
+#three
+
+#if elif in simple form :
+#	
+x = 3
+if x == 2:
+    print('two')
+if x == 3:
+    print('three')
+if x == 4:
+    print('four')
+#output:
+#three
