796359a549c2fb68ac73500f79748e62f57251a8
Balagokul <84243841+Balagokul01@users.noreply.github.com>
2021-06-30 19:03:53 +0530

    Create If statement.py
    If statement with comments

---
+++ Control Structures/If statement.py
@@ -0,0 +1,10 @@
+x=5
+#x is a variable which has value 5
+
+if x>0:
+#This is if statement with condition here as x should be greater than zero, if x is greater than zero it satisfies statement and prints below statement
+	print(x," is greater than 0")
+	
+	
+	#output:
+	#5 is greater than 0
