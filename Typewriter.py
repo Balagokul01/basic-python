fd8be9e516848426241c179e3fed78b2ab9dd42d
Balagokul <84243841+Balagokul01@users.noreply.github.com>
2024-01-26 04:19:48 +0530

    Update Typewriter.py
    changing function name for test

--- Typewriter.py
+++ Typewriter.py
@@ -1,12 +1,12 @@
 #shows text word by word like it's typed
 from time import sleep
-def printt(string, delay):
+def printte(string, delay):
 	for i in str(string):
 		print(i, end="", flush=True)
 		sleep(delay)
 	print('')
 
 
-printt('This string will be "typed" out letter by letter.\nPretty cool, right?',0.05)
-printt(f'Here is an example of typing out an integer\n{1234}\n', 0.08)
-printt(f'And a float: {1.234}', 0.0)
+printte('This string will be "typed" out letter by letter.\nPretty cool, right?',0.05)
+printte(f'Here is an example of typing out an integer\n{1234}\n', 0.08)
+printte(f'And a float: {1.234}', 0.0)
